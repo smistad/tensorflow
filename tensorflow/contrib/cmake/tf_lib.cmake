b061c47880a75b39f694b48694fd0e7ca4b5fcee
Erik Smistad <ersmistad@gmail.com>
2017-01-09 12:55:15 +0100

    Fixed tensorflow lib build to work on linux again

--- tensorflow/contrib/cmake/tf_lib.cmake
+++ tensorflow/contrib/cmake/tf_lib.cmake
@@ -1,93 +1,20 @@
 # Create a tensorflow library, and set up rule for install the library and necessary headers for C++
-
-########################################################
-# RELATIVE_PROTOBUF_GENERATE_CPP function
-########################################################
-# A variant of PROTOBUF_GENERATE_CPP that keeps the directory hierarchy.
-# ROOT_DIR must be absolute, and proto paths must be relative to ROOT_DIR.
-function(RELATIVE_PROTOBUF_GENERATE_CPP SRCS HDRS ROOT_DIR)
-  if(NOT ARGN)
-    message(SEND_ERROR "Error: RELATIVE_PROTOBUF_GENERATE_CPP() called without any proto files")
-    return()
-  endif()
-  
-  set(${SRCS})
-  set(${HDRS})
-  foreach(FIL ${ARGN})
-    set(ABS_FIL ${ROOT_DIR}/${FIL})
-    get_filename_component(FIL_WE ${FIL} NAME_WE)
-    get_filename_component(FIL_DIR ${ABS_FIL} PATH)
-    file(RELATIVE_PATH REL_DIR ${ROOT_DIR} ${FIL_DIR})
-
-    list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.cc")
-    list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.h")
-
-    add_custom_command(
-      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.cc"
-             "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.h"
-      COMMAND  ${PROTOBUF_PROTOC_EXECUTABLE}
-      ARGS --cpp_out  ${CMAKE_CURRENT_BINARY_DIR} -I ${ROOT_DIR} ${ABS_FIL} -I ${PROTOBUF_INCLUDE_DIRS}
-      DEPENDS ${ABS_FIL} protobuf
-      COMMENT "Running C++ protocol buffer compiler on ${FIL}"
-      VERBATIM )
-  endforeach()
-
-  set_source_files_properties(${${SRCS}} ${${HDRS}} PROPERTIES GENERATED TRUE)
-  set(${SRCS} ${${SRCS}} PARENT_SCOPE)
-  set(${HDRS} ${${HDRS}} PARENT_SCOPE)
-endfunction()
-
-function(RELATIVE_PROTOBUF_TEXT_GENERATE_CPP SRCS HDRS ROOT_DIR)
-  if(NOT ARGN)
-      message(SEND_ERROR "Error: RELATIVE_PROTOBUF_TEXT_GENERATE_CPP() called without any proto files")
-    return()
-  endif()
-
-  set(${SRCS})
-  set(${HDRS})
-  foreach(FIL ${ARGN})
-    set(ABS_FIL ${ROOT_DIR}/${FIL})
-    get_filename_component(FIL_WE ${FIL} NAME_WE)
-    get_filename_component(FIL_DIR ${ABS_FIL} PATH)
-    file(RELATIVE_PATH REL_DIR ${ROOT_DIR} ${FIL_DIR})
-
-    list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb_text.cc")
-    list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb_text.h")
-
-    add_custom_command(
-      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb_text.cc"
-             "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb_text.h"
-      COMMAND ${PROTO_TEXT_EXE}
-      ARGS "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}" ${REL_DIR} ${ABS_FIL} "${ROOT_DIR}/tensorflow/tools/proto_text/placeholder.txt"
-      DEPENDS ${ABS_FIL} ${PROTO_TEXT_EXE}
-      COMMENT "Running C++ protocol buffer text compiler (${PROTO_TEXT_EXE}) on ${FIL}"
-      VERBATIM )
-  endforeach()
-
-  set_source_files_properties(${${SRCS}} ${${HDRS}} PROPERTIES GENERATED TRUE)
-  set(${SRCS} ${${SRCS}} PARENT_SCOPE)
-  set(${HDRS} ${${HDRS}} PARENT_SCOPE)
-endfunction()
-
-########################################################
-# tf_protos_cc library
-########################################################
-
-file(GLOB_RECURSE tf_protos_cc_srcs RELATIVE ${tensorflow_source_dir}
-    "${tensorflow_source_dir}/tensorflow/core/*.proto"
-)
-RELATIVE_PROTOBUF_GENERATE_CPP(PROTO_SRCS PROTO_HDRS
-    ${tensorflow_source_dir} ${tf_protos_cc_srcs}
-)
-
-
 if(WIN32)
+    file(GLOB_RECURSE tf_protos_cc_srcs RELATIVE ${tensorflow_source_dir}
+        "${tensorflow_source_dir}/tensorflow/core/*.proto"
+    )
+    RELATIVE_PROTOBUF_GENERATE_CPP(PROTO_SRCS PROTO_HDRS
+        ${tensorflow_source_dir} ${tf_protos_cc_srcs}
+    )
     # Build static library on windows
     # Unable to create DLL on windows with MSVC
     set(BUILD_SHARED_LIBS OFF)
     # Build with multiple processes on windows
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
-endif()
+else(WIN32)
+    # Build shared lib on other systems than windows
+    set(BUILD_SHARED_LIBS ON)
+endif(WIN32)
 
 add_library(tensorflow
     "${tensorflow_source_dir}/tensorflow/c/c_api.cc"
@@ -104,8 +31,8 @@ add_library(tensorflow
     $<$<BOOL:${tensorflow_ENABLE_GRPC_SUPPORT}>:$<TARGET_OBJECTS:tf_core_distributed_runtime>>
     $<TARGET_OBJECTS:tf_core_kernels>
     $<$<BOOL:${tensorflow_ENABLE_GPU}>:$<TARGET_OBJECTS:tf_stream_executor>>
-    ${PROTO_HDRS}
-    ${PROTO_SRCS}
+    $<$<BOOL:WIN32>:${PROTO_HDRS}>
+    $<$<BOOL:WIN32>:${PROTO_SRCS}>
 )
 
 # Install files
@@ -115,6 +42,19 @@ install(
     tensorflow
     DESTINATION lib
 )
+
+if(NOT WIN32)
+    # Besides windows, we link any dependencies
+    target_link_libraries(tensorflow
+        ${tf_core_gpu_kernels_lib}
+        ${tensorflow_EXTERNAL_LIBRARIES}
+        tf_protos_cc
+    )
+    add_dependencies(tensorflow
+        ${tensorflow_EXTERNAL_DEPENDENCIES}
+        tf_protos_cc
+    )
+endif(NOT WIN32)
 
 # 2) TF headers
 install(
@@ -146,26 +86,34 @@ install(
 )
 
 # 5) External libraries
-install(
-	FILES
-    ${zlib_STATIC_LIBRARIES}
-    ${gif_STATIC_LIBRARIES}
-    ${png_STATIC_LIBRARIES}
-    ${jpeg_STATIC_LIBRARIES}
-    ${jsoncpp_STATIC_LIBRARIES}
-    ${farmhash_STATIC_LIBRARIES}
-    ${highwayhash_STATIC_LIBRARIES}
-    ${protobuf_STATIC_LIBRARIES}
-	DESTINATION lib
+if(WIN32)
+    # For windows, since we are building a static lib,
+    # we also need the static dependencies
+    install(
+        FILES
+        ${zlib_STATIC_LIBRARIES}
+        ${gif_STATIC_LIBRARIES}
+        ${png_STATIC_LIBRARIES}
+        ${jpeg_STATIC_LIBRARIES}
+        ${jsoncpp_STATIC_LIBRARIES}
+        ${farmhash_STATIC_LIBRARIES}
+        ${highwayhash_STATIC_LIBRARIES}
+        ${protobuf_STATIC_LIBRARIES}
+        DESTINATION lib
+    )
+endif(WIN32)
+
+# Write a cmake config file, listing all libraries and include dirs
+install(CODE "
+    set(CONF_INCLUDE_DIRS \"${CMAKE_INSTALL_PREFIX}include/\")
+    file(GLOB CONF_LIBRARY \"${CMAKE_INSTALL_PREFIX}lib/*tensorflow.*\")
+    file(GLOB CONF_LIBRARIES \"${CMAKE_INSTALL_PREFIX}lib/*\")
+    configure_file(TensorflowConfig.cmake.in \"${PROJECT_BINARY_DIR}/TensorflowConfig.cmake\" @ONLY)
+    "
 )
 
-# Write a cmake config file, listing all libraries
-set(CONF_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/")
-file(GLOB CONF_LIBRARY "${CMAKE_INSTALL_PREFIX}/lib/tensorflow.lib")
-file(GLOB CONF_LIBRARIES "${CMAKE_INSTALL_PREFIX}/lib/*")
-configure_file(TensorflowConfig.cmake.in "${PROJECT_BINARY_DIR}/TensorflowConfig.cmake" @ONLY)
 install(
-	FILES
-	"${PROJECT_BINARY_DIR}/TensorflowConfig.cmake"
-	DESTINATION cmake
+    FILES
+    "${PROJECT_BINARY_DIR}/TensorflowConfig.cmake"
+    DESTINATION cmake
 )
